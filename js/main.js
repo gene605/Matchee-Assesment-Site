f1727d83d0435fd14f2845e131f83ccd8135cc32
gene605 <geneterryr@gmail.com>
2023-11-07 16:49:44 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+console.log('test');
+// window.addEventListener ('load', function() {
+//     console.log('test');
+//     fetch('/templates/header.html')
+//     .then(response => {console.log(response); return response.text()})
+//     .then(data => document.getElementById('header').innerHTML = data);
+
+//     fetch('/templates/footer.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('footer').innerHTML = data);
+
+//     fetch('/pages/page-home.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('main-content').innerHTML = data);
+// });
